7c3d6230db153f005dad4d4bf72a98ee13701b42
RozsaDavid <rozsadavid0415@gmail.com>
2024-12-08 09:25:02 +0100

    hibakezelés, hibakijelzés

--- FrontEnd/ViewModel/UserVM.cs
+++ FrontEnd/ViewModel/UserVM.cs
@@ -6,11 +6,14 @@ namespace FrontEnd.ViewModel {
 
         public string Username { get; set; } = null!;
 
+        /*[RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+            ErrorMessage = "Adj meg érvényes jelszót! (Minimum 8 karakter, kisbetű, nagybetű, szám és speciális karaktert tartalmaznia kell.)")]*/
         public string Password { get; set; } = null!;
 
         public string RealName { get; set; } = null!;
 
-        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Adj meg érvényes e-mail címet, pl.: \"pelda@domain.com\" .")]
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+            ErrorMessage = "Adj meg érvényes e-mail címet, pl.: \"pelda@domain.com\" .")]
         public string Email { get; set; } = null!;
 
         public sbyte? IsActive { get; set; }
