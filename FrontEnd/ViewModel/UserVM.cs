c984ad5cecfb80e78edbe45e972985a495950cf1
RozsaDavid <rozsadavid0415@gmail.com>
2024-12-08 06:55:58 +0100

    css errormessage

--- FrontEnd/ViewModel/UserVM.cs
+++ FrontEnd/ViewModel/UserVM.cs
@@ -1,4 +1,6 @@
-﻿namespace FrontEnd.ViewModel {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.ViewModel {
     public class UserVM {
         public int Id { get; set; }
 
@@ -8,6 +10,7 @@
 
         public string RealName { get; set; } = null!;
 
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Adj meg érvényes e-mail címet, pl.: \"pelda@domain.com\" .")]
         public string Email { get; set; } = null!;
 
         public sbyte? IsActive { get; set; }
